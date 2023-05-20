081db9314d9bcf75661d2722cb77376e77296b78
ivamitch <118473358+ivamitch@users.noreply.github.com>
2023-05-20 14:32:02 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,9 +25,27 @@ ulLink.addEventListener('click', onClick)
 
 function onClick(e) {
     e.preventDefault();
-    if (e.target.classList.contains('gallery__image')) {
+    if (!e.target.classList.contains('gallery__image')) {
         return
     }
+
+
+    const instance  = basicLightbox.create(
+        `<img src="${e.target.dataset.source}" width="800" height="600">`
+        );
+
+    instance.show();
+
+    // Закриття модального вікна при натисканні Escape
+
+    ulLink.addEventListener('keydown', closeBigImage);
+
+  function closeBigImage(event) {
+    if (event.code === 'Escape') {
+      instance.close(ulLink.removeEventListener('keydown', closeBigImage));
+    }
+  }
 }
+
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const ulLink = document.querySelector(".gallery");
+ulLink.insertAdjacentHTML("beforeend", createImageInGallry(galleryItems));
+
+function createImageInGallry(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+        `).join("");
+}
+
+const myGallery = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 300,
+});
